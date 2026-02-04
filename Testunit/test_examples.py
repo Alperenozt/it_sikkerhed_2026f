17e2ecadae64dca2324cce1b5522f9098dc40b58
Alo001 <alo001@edu.zealand.dk>
2026-02-04 09:04:07 +0100

    fejlede tests

--- Testunit/test_examples.py
+++ Testunit/test_examples.py
@@ -1,20 +1,27 @@
 import pytest
 
-# ─────────────── Basis tests ─────────────── 
+def test_pass():
+    assert True
+
 def test_fail():
-    assert "Python" == "Python"
+    # Fejler pga. stavefejl
+    assert "Python" == "pyhton"
+
+def test_skip():
+    pytest.skip("Denne test springes over med vilje")
 
 def test_crash():
-    value = "python"
-    assert value.upper() == "PYTHON"
+    value = None
+    value.upper()  # AttributeError
 
 def test_fail_list_length():
     data = [1, 2, 3]
-    assert len(data) == 3
+    assert len(data) == 5
 
 def test_crash_keyerror():
-    user = {"navn": "Alperen", "alder": 18}
-    assert user["alder"] == 18
+    user = {"navn": "Alperen"}
+    user["alder"]  # KeyError
 
 def test_fail_math():
-    assert 10 / 2 == 5
+    assert 10 / 2 == 3
+
